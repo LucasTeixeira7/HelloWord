86fdb6fc7c6dd956dd2037b0858f8214b601fbf3
Lucas Teixeira <lucas01teixeira01@gmail.com>
2024-04-15 20:37:10 -0300

    primeiro commit :)

---
+++ HelloWord.py
@@ -0,0 +1,36 @@
+""" 
+Exemplo: 001
+Objetivo: O famoso Hello Word
+Programador: Lucas Teixeira
+Data: 06/03/2023
+Versão 001
+"""
+
+print("minha primeira linha em Python")
+
+x = 1
+X = 5.983883
+xX = "Unicid"
+
+print (x)
+print (X)
+print (xX)
+
+x = str(9)
+y = int(9)
+z = float(9)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+p = "O Brasil é demais"
+print(p)
+
+a = 323
+
+print((a+10)**2)
+
+NomeAluno = input("Digite o nome do aluno")
+
+print(NomeAluno)
